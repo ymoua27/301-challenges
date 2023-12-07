adbaf14db5f1f3ebe89c5fffebd259242432e713
Yue Moua <ymoua27@gmail.com>
2023-12-06 21:41:18 -0800

    initial

---
+++ collect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#Functions 
+my_list = ["red", "blue", "green", "yellow", "purple", "black", "brown", "gray", "orange", "pink"]
+
+# Print the fourth element of the list
+print("4:", my_list[3])
+
+# Print the sixth through tenth element of the list
+print("6-10:", my_list[5:10])
+
+# Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+
+# Print the updated list
+print("Reverse:", my_list)
